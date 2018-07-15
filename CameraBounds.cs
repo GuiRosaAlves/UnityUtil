488a55af92b8b0f1a2e4be37cc8fa0d32ab318ae
Guilherme Rosa Alves <guilhermealves1954@hotmail.com>
2018-07-14 21:10:03 -0300

    Camera scripts
    Added:
    - Camera following script
    - Camera bounds (Only works with a ortographic camera)

---
+++ CameraBounds.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Only works with ortographic camera
+public class CameraBounds : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraBounds;
+    private Vector2 _boundsSize;
+    private Vector2 _boundsOffset;
+    private float _left, _right, _top, _bottom;
+
+    void Awake ()
+    {
+        if (_cameraBounds)
+        {
+            Sprite sprite = _cameraBounds.GetComponent<SpriteRenderer>().sprite;
+            float pixelPerUnits = sprite.rect.width / sprite.bounds.size.x;
+            CalculateSize(sprite, pixelPerUnits);
+            RefreshBounds();
+        }
+        else
+        {
+            Debug.Log("The camera does not have boundaries!");
+        }
+    }
+	
+	void LateUpdate ()
+    {
+        if (_cameraBounds)
+        {
+            Vector3 v3 = transform.position;
+            v3.x = Mathf.Clamp(v3.x, _left, _right);
+            v3.y = Mathf.Clamp(v3.y, _bottom, _top);
+            transform.position = v3;
+
+            RefreshBounds();
+        }
+    }
+
+    protected void CalculateSize(Sprite sprite, float pixelPerUnits)
+    {
+        _boundsSize = new Vector2(_cameraBounds.transform.localScale.x * sprite.texture.width / pixelPerUnits,
+            _cameraBounds.transform.localScale.y * sprite.texture.height / pixelPerUnits);
+        _boundsOffset = _cameraBounds.transform.position;
+    }
+
+    protected void RefreshBounds()
+    {
+        var vertExtent = Camera.main.orthographicSize;
+        var horzExtent = vertExtent * Screen.width / Screen.height;
+
+        _left = horzExtent - _boundsSize.x / 2.0f + _boundsOffset.x;
+        _right = _boundsSize.x / 2.0f - horzExtent + _boundsOffset.x;
+        _bottom = vertExtent - _boundsSize.y / 2.0f + _boundsOffset.y;
+        _top = _boundsSize.y / 2.0f - vertExtent + _boundsOffset.y;
+    }
+}
---
+++ CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _camOffset = new Vector3(0, 0, -10);
+    [Range(1f,10f)] [SerializeField] private float _smoothTimeX = 1f, _smoothTimeY = 1f, _smoothTimeZ = 1f;
+    private Vector3 _velocity;
+    public Camera Camera { get; private set; }
+
+    void Awake()
+    {
+        Camera = GetComponent<Camera>();
+        if (!Camera)
+        {
+            Debug.Log("Attach this component to a Camera object!");
+        }
+    }
+    void FixedUpdate()
+    {
+        if (_target == null)
+        {
+            Debug.Log("There is no target to follow!");
+        }else
+        {
+            float posX = Mathf.SmoothDamp(transform.position.x, _target.position.x + _camOffset.x, ref _velocity.x, 1/_smoothTimeX);
+            float posY = Mathf.SmoothDamp(transform.position.y, _target.position.y + _camOffset.y, ref _velocity.y, 1/_smoothTimeY);
+            float posZ = Mathf.SmoothDamp(transform.position.z, _target.position.z + _camOffset.z, ref _velocity.z, 1 / _smoothTimeZ);
+
+            transform.position = (Vector3.right * posX) + (Vector3.up * posY)  + (Vector3.forward * posZ);
+        }
+    }
+
+    public Transform Target() { return _target; }
+    public Vector3 CamOffset() { return _camOffset; }
+    public float SmoothTimeX() { return _smoothTimeX; }
+    public float SmoothTimeY() { return _smoothTimeY; }
+    public float SmoothTimeZ() { return _smoothTimeZ; }
+}
